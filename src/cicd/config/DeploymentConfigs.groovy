bbb1ad9fbc3b577e7804c760d168792eb2a3f38a
Mihai <mihaimihai.prut@gmail.com>
2023-01-18 13:10:48 +0100

    Load global configs Load shared library

--- src/cicd/config/DeploymentConfigs.groovy
+++ src/cicd/config/DeploymentConfigs.groovy
@@ -3,4 +3,16 @@ package cicd.config
 class DeploymentConfigs {
 
   public static def jobInstance = null;
+
+  /**
+   * Configuration from jenkins-lib <b>resources/global-configs.yaml</b>
+   */
+  public static Map globalConfigs = null;
+
+  public static Map loadGlobalConfigs() {
+    final String globalConfigsResource = DeploymentConfigs.jobInstance.libraryResource("global-configs.yaml");
+    DeploymentConfigs.globalConfigs = DeploymentConfigs.jobInstance.readYaml(text: globalConfigsResource);
+    DeploymentConfigs.jobInstance.echo("DEBUG: " + DeploymentConfigs.globalConfigs); // TODO Remove it after debug
+    return DeploymentConfigs.globalConfigs;
+  }
 }
