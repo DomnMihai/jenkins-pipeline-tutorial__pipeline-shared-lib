c8d48489348e2021a56670cf6bcd52b9c13880d5
Mihai <mihaimihai.prut@gmail.com>
2023-01-29 16:40:46 +0100

    Removed debug log Clone project repository

--- src/cicd/config/DeploymentConfigs.groovy
+++ src/cicd/config/DeploymentConfigs.groovy
@@ -12,7 +12,6 @@ class DeploymentConfigs {
   public static Map loadGlobalConfigs() {
     final String globalConfigsResource = DeploymentConfigs.jobInstance.libraryResource("global-configs.yaml");
     DeploymentConfigs.globalConfigs = DeploymentConfigs.jobInstance.readYaml(text: globalConfigsResource);
-    DeploymentConfigs.jobInstance.echo("DEBUG: " + DeploymentConfigs.globalConfigs); // TODO Remove it after debug
     return DeploymentConfigs.globalConfigs;
   }
 }
